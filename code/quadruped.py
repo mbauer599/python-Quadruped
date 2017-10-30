3189a823834dd9ae20bd14d5f7d13bfb30b42cc0
winterfox59 <mbauer599@users.noreply.github.com>
2017-10-30 15:13:01 -0500

    Added one_step_forward function.

--- code/quadruped.py
+++ code/quadruped.py
@@ -34,3 +34,37 @@ servo_max = 600  # Max pulse length out of 4096
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 ############################
+
+# Helper function to make setting a servo pulse width simpler.
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
+
+# Helper function to take one full step forward.
+def one_step_forward():
+    pwm.set_pwm(channel_FR1B, 0, servo_max)
+    pwm.set_pwm(channel_RL3B, 0, servo_max)
+    pwm.set_pwm(channel_FR1A, 0, servo_max)
+    pwm.set_pwm(channel_RL3A, 0, servo_max)
+    time.sleep(1)
+    pwm.set_pwm(channel_FR1B, 0, servo_min)
+    pwm.set_pwm(channel_RL3B, 0, servo_min)
+    pwm.set_pwm(channel_FR1A, 0, servo_min)
+    pwm.set_pwm(channel_RL3A, 0, servo_min)
+    time.sleep(1)
+    pwm.set_pwm(channel_FL4B, 0, servo_max)
+    pwm.set_pwm(channel_RR2B, 0, servo_max)
+    pwm.set_pwm(channel_FL4A, 0, servo_max)
+    pwm.set_pwm(channel_RR2A, 0, servo_max)
+    time.sleep(1)
+    pwm.set_pwm(channel_FL4B, 0, servo_min)
+    pwm.set_pwm(channel_RR2B, 0, servo_min)
+    pwm.set_pwm(channel_FL4A, 0, servo_min)
+    pwm.set_pwm(channel_RR2A, 0, servo_min)
+    time.sleep(1)
