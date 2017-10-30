bb80bb59e68407864de1c7258ba12c642a1d5d32
winterfox59 <mbauer599@users.noreply.github.com>
2017-10-30 14:40:34 -0500

    Added Global Variables for servos

--- code/quadruped.py
+++ code/quadruped.py
@@ -1,4 +1,20 @@
 #!/bin/python
 # Written by Michael Bauer
-#
 
+############################
+##### Global Variables #####
+############################
+# Servo Mappings
+# Front Right Servos: 1A/1B Channel: 0/4
+channel_FR1A = 0
+channel_FR1B = 4
+# Rear Right Servos: 2A/2B  Channel: 1/5
+channel_RR2A = 1
+channel_RR2B = 5
+# Rear Left Servos: 3A/3B   Channel: 2/6
+channel_RL3A = 2
+channel_RL3B = 6
+# Front Left Servos: 4A/4B  Channel: 3/7
+channel_FL4A = 0
+channel_FL4B = 4
+############################
