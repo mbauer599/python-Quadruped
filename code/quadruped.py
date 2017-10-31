6c7df64a6062bce1b1dafeb65919295baffc8640
winterfox59 <mbauer599@users.noreply.github.com>
2017-10-31 15:43:33 -0500

    Added distance function

--- code/quadruped.py
+++ code/quadruped.py
@@ -62,6 +62,34 @@ def set_servo_pulse(channel, pulse):
     pulse //= pulse_length
     pwm.set_pwm(channel, 0, pulse)
 
+# Function to return distance
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+    
+    return distance
+    
 # Helper function to take one full step forward.
 def one_step_forward():
     pwm.set_pwm(channel_FR1B, 0, servo_max)
