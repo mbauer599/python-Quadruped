2916c546b78daa907e38f6797ee442e394fa7c9b
winterfox59 <mbauer599@users.noreply.github.com>
2017-11-01 09:47:35 -0500

    Root or Sudo check

--- code/install.sh
+++ code/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Simple script to install all dependencies and get up and going. 
 
+# Make sure we're root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # Update the host system
 apt-get update
